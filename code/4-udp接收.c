103ffde129872f9fa5337b199c3084cd380719c3
MatthewVork <wzy1845089791@gmail.com>
2026-01-19 20:31:24 +0800

    添加注释

--- code/4-udp接收.c
+++ code/4-udp接收.c
@@ -20,13 +20,26 @@ int main(void)
     memset(&addr, 0, sizeof(addr));//清空
     addr.sin_family = AF_INET;//初始化协议族
     addr.sin_port = htons(9999);
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);   //绑定内地址
     int ret  = bind(sockfd, (struct sockaddr*)&addr, sizeof(addr));
     if(ret < 0)
     {
         perror("bind");
         return -1;
     }
+
+    /*
+    
+    绑定函数
+
+    ssize_t bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen)
+    返回值：成功 -> 0 失败 -> -1
+
+    参数:   int sockfd 套接字
+            const struct sockaddr *addr 地址
+            socklen_t addrlen 地址长度
+
+    */
 
     //接收数据
     char buffer[128]={0};
@@ -37,6 +50,21 @@ int main(void)
     {
         perror("recvfrom");
     }
+
+    /*
+    
+    接收函数
+    ssize_t recvfrom(int sockfd, void *buf, size_t len, int flags, struct sockaddr *src_addr, socklen_t *addrlen);
+    返回值：成功 -> 接受数据大小 失败 -> -1
+    
+    参数：int sockfd 套接字
+          void *buf 储存数据的首地址
+          size_t len 储存数据的空间大小
+          int flag 标志位，一般为0
+          struct sockaddr *src_addr 保存发送方的地址
+          socklen_t *addrlen 地址长度
+
+    */
     printf("%s-%d-%s\n",inet_ntoa(caddr.sin_addr), ntohs(caddr.sin_port), buffer);
 
     //关闭套接字
